26133c855e77d82560bd1ddc2e25b65d675c3eff
hangilk <hangilxk24@sherwoodsd.com>
2022-11-14 12:04:28 -0800

    Resolved nullpointer with commit below

--- src/com/sherwoodhs/ui/TankStatusBar.java
+++ src/com/sherwoodhs/ui/TankStatusBar.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Objects;
 
 public class TankStatusBar extends JPanel implements ActionListener, ItemListener, ChangeListener {
 
@@ -257,7 +258,7 @@ public class TankStatusBar extends JPanel implements ActionListener, ItemListene
         //  This method is called when the combo box has been set to a new item.
         //  Reset the amount that is available.
         currentTank.setSelectedWeapon((String) artillery.getSelectedItem());
-        if (artillery.getSelectedItem().equals("Normal Bullet")) {
+        if (Objects.equals(artillery.getSelectedItem(), "Normal Bullet")) {
             amountValue.setText("" + "inf");
         } else {
             amountValue.setText("" + currentTank.getWeaponsCount().get(currentTank.getSelectedWeapon()));
