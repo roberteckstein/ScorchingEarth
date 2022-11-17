3d3b0941422e759c42761065750c5f777e1b4f0e
thederpcodeman <connertm24@sherwoodsd.com>
2022-11-17 11:57:58 -0800

    drill no longer fires a completely different weapon

--- src/com/sherwoodhs/ui/TankStatusBar.java
+++ src/com/sherwoodhs/ui/TankStatusBar.java
@@ -253,7 +253,7 @@ public class TankStatusBar extends JPanel implements ActionListener, ItemListene
                 }else if (artillery.getSelectedItem().equals(("Firework"))) {
                     game.bullets.add(new Firework(game, (int) x, (int) (y - 10), dx, dy, 1));
                 }else if (artillery.getSelectedItem().equals(("Drill"))) {
-                    game.bullets.add(new Firework(game, (int) x, (int) (y - 10), dx, dy, 0.75));
+                    game.bullets.add(new Drill(game, (int) x, (int) (y - 10), dx, dy, 0.75));
                 }
                 //  Set the boolean in the game object that the fire button has been pressed.
                 //  At this point, the main thread will start processing animating objects.
